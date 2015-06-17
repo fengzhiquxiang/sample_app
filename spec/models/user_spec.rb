6a7186c52efb522a3642b06ea866721d7d5796f5
fengzhiquxiang <fengzhiquxiang@gmail.com>
2015-06-17 22:54:41 +0800

    Make a basic User model (including secure passwords)2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -76,4 +76,22 @@ password_confirmation: "foobar") }
 			specify { expect(user_for_invalid_password).to be_false }
 		end
 	end
+
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
+		end
+	end
+
+	describe "email address has not dot connect" do
+		let(:connect_case_email) { "Foo@ExAMPle..CoM" }
+		it "should be invalid" do
+			@user.email = connect_case_email
+			expect(@user.save).to be_false
+			# expect(@user.save).should_not be_valid
+		end
+	end
 end
