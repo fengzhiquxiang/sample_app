b7c99dafde29ec2910db55fb9f0562e7bbf85aee
fengzhiquxiang <fengzhiquxiang@gmail.com>
2015-06-18 23:00:48 +0800

    modify debug message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+  	@uu = User.find_by_email("michael@example.com")
+  end
+
+  def index
+  	@us = User.all
+  end
 end
