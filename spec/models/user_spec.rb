d785ebc446b32b800bd0b063fd262554e637c4d1
fengzhiquxiang <fengzhiquxiang@gmail.com>
2015-07-08 16:17:32 +0800

    User micropost associations should have the right microposts in the right order

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  	before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
+  before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 	subject { @user }
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
@@ -11,6 +11,7 @@ describe User do
 	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 	it { should respond_to(:admin) }
+	it { should respond_to(:microposts) }
 
 	it { should be_valid }
 	it { should_not be_admin }
@@ -108,5 +109,18 @@ describe User do
 			@user.toggle!(:admin)
 		end
 		it { should be_admin }
+	end
+
+	describe "micropost associations" do
+		before { @user.save }
+		let!(:older_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+		end
+		let!(:newer_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+		end
+		it "should have the right microposts in the right order" do
+			expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+		end
 	end
 end
